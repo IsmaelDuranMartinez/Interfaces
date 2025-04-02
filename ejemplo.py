8b10b5bcd60579d7aca90fec059013cb0955488a
Ismaelduranm <A01731393@tec.mx>
2025-04-01 18:11:58 -0600

    agregar hola

---
+++ ejemplo.py
@@ -0,0 +1,8 @@
+print("hola a todos, este es un repositorio local y se desplegara en git hub")
+print("Este archivo se estara modificando ")
+
+for i  in range (10):
+    print(i)
+
+
+print("hola")
